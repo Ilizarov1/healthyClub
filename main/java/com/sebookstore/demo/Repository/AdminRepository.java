bd4097e52d2226b7c13e523489f9d7a3acf71059
Ilizarov1 <1224722105@qq.com>
2019-11-23 10:58:08 +0800

    删除多余的文件夹

--- main/java/com/sebookstore/demo/Repository/AdminRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sebookstore.demo.Repository;
-
-import com.sebookstore.demo.Entity.AdminUser;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AdminRepository extends JpaRepository<AdminUser,Long>
-{
-    AdminUser findByAdminName(String adminName);
-}
--- main/java/com/sebookstore/demo/Service/AdminService.java
+++
@@ -1,8 +0,0 @@
-package com.sebookstore.demo.Service;
-
-import com.sebookstore.demo.Entity.AdminUser;
-
-public interface AdminService
-{
-    AdminUser findByAdminName(String adminName);
-}
